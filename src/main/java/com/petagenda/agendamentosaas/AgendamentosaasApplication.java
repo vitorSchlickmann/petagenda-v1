75f3e375e419fca7b6f0660234808f97923e866a
vitorSchlickmann <vitorsluciano@gmail.com>
2025-06-12 21:17:22 -0300

    criação do projeto

---
+++ src/main/java/com/petagenda/agendamentosaas/AgendamentosaasApplication.java
@@ -0,0 +1,13 @@
+package com.petagenda.agendamentosaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentosaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentosaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petagenda/agendamentosaas/AgendamentosaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petagenda.agendamentosaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
